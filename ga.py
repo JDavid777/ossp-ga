45f665addbda71ca0a9d94a2cfe42b5f3a25a211
JDavid777 <jdavidgarcia28@gmail.com>
2022-01-24 16:09:14 -0500

    se agrega operador de cruce personalizado

--- ga.py
+++ ga.py
@@ -43,6 +43,30 @@ ossp = OsspProblem(MATRIX,3)
 
 """OSSP"""
 
+def cxTwoPoint(ind1, ind2):
+    """Executes a two-point crossover on the input :term:`sequence`
+    individuals. The two individuals are modified in place and both keep
+    their original length.
+
+    :param ind1: The first individual participating in the crossover.
+    :param ind2: The second individual participating in the crossover.
+    :returns: A tuple of two individuals.
+
+    This function uses the :func:`~random.randint` function from the Python
+    base :mod:`random` module.
+    """
+    size = min(len(ind1), len(ind2))
+    cxpoint1 = random.randint(1, size)
+    cxpoint2 = random.randint(1, size - 1)
+    if cxpoint2 >= cxpoint1:
+        cxpoint2 += 1
+    else:  # Swap the two cx points
+        cxpoint1, cxpoint2 = cxpoint2, cxpoint1
+
+    ind1[cxpoint1:cxpoint2], ind2[cxpoint1:cxpoint2] \
+        = ind2[cxpoint1:cxpoint2], ind1[cxpoint1:cxpoint2]
+
+    return ind1, ind2
 
 def setup_ga_ossp( indpb = 0.2):
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
